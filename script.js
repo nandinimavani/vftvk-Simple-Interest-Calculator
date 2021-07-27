0d6997d0c046a0688fe577326ec97a24827a236b
nandinimavani <nandini.mavani.1998@gmail.com>
2021-07-27 14:46:10 +0000

    simple interest calculator final project

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function compute()
 		return;
 	}
     var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
+    var years = document.getElementById("years").value; /years of interest/
     var interest = principal * years * rate /100; /calculate interest/
     var year = new Date().getFullYear()+parseInt(years); /years/
     document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
