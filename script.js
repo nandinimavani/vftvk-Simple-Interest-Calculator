72ec9f4caffc7266681d8ce4081a0271bff521c0
nandinimavani <nandini.mavani.1998@gmail.com>
2021-07-27 15:10:08 +0000

    final project

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
 function showMessage() {
 	document.getElementById("result").style.display = "block";
 };
-function compute()
-{
-    var principal = document.getElementById("principal").value;
-    if(parseInt(principal) < 1) {
+
+function compute() {
+	var principal = document.getElementById("principal").value;
+	if(parseInt(principal) < 1) {
 		alert("Enter a positive number");
 		document.getElementById("principal").focus();
 		return;
 	}
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value; /years of interest/
-    var interest = principal * years * rate /100; /calculate interest/
-    var year = new Date().getFullYear()+parseInt(years); /years/
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"; showMessage();
-    return false;
-}
 
-function updateRate() 
-{
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+
+	var rate = document.getElementById("rate").value;
+	var years = document.getElementById("years").value;
+
+	var interest = principal * years * rate / 100;
+
+	var today = new Date();
+	var yearOfWithdrawal = parseInt(today.getFullYear())+parseInt(years);
+
+	document.getElementById("depositmessage").innerHTML = ""+principal;
+	document.getElementById("interestmessage").innerHTML = ""+rate;
+	document.getElementById("amountmessage").innerHTML = ""+interest;
+	document.getElementById("yearmessage").innerHTML = ""+yearOfWithdrawal;
+	showMessage();
+
+	return false;
+};
+
+function showRange() {
+	var rate = document.getElementById("rate").value;
+	document.getElementById("ratevalue").innerHTML = rate;
 }
-        
